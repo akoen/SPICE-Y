fca4984e6283cf4415d6a5d8143192aca0d802e2
mjohal67 <majohal67@gmail.com>
2022-06-21 19:31:53 -0700

    Updated motor pwm pins

--- src/main.cpp
+++ src/main.cpp
@@ -1,92 +1,99 @@
+
+
+/**
+* @brief Blink: Turns on an LED for one second then off for
+* one second and then repeats.
+*/
+
 #include "Arduino.h"
 
-// Set LED_BUILTIN if it is not defined by Arduino framework
+//Set LED_BUILTIN if it is not defined by Arduino framework
 #define LED_BUILTIN PC13
+#define PWM_PIN PB6
+#define TAPE_SNS PA7
+#define IR_SNS PA6
+#define PULSE_SONAR PB11
+#define SONAR_SNS PB0
 #define PWM_FWD PA_3
-#define PWM_REV PA_7
-#define DUTY_CYCLE_ADJ PB1
-#define DIRECTION_CTRL PB10
+#define PWM_BACK PA_7
 
-#define FULL_SCALE_ADC 1024
-#define FULL_SCALE_PWM 65535
-#define PWM_FREQ 100
+#define PWM_FREQ 10
 
-#define FWD 1
-#define REV 0
-
-HardwareTimer *HB_TIM = new HardwareTimer(TIM1);
-HardwareTimer *M1_PWM_TIM = new HardwareTimer(TIM2);
-volatile bool led_state = false;
-
-void Heartbeat()
-{
-  led_state = !led_state;
-  digitalWrite(LED_BUILTIN, led_state ? HIGH : LOW);
-}
-
-void testMotor()
-{
-  // placeholder fn
-}
-
-void UpdateMotor1State()
-{
-
-  // call this function every x times a second - how many?
-
-  int direction = digitalRead(DIRECTION_CTRL);
-
-  /* uncomment once you have a pot hooked up to this adc channel
-  float duty_cycle_percent = analogRead(DUTY_CYCLE_ADJ) / FULL_SCALE_ADC; //number between 0 - 1023
-  float pwm_duty_cycle = duty_cycle_percent * FULL_SCALE_PWM;
-  */
-
-  // comment this out when you've hooked up adc channel pot duty cycle control
-  float pwm_duty_cycle = FULL_SCALE_PWM * 0.5;
-
-  switch (direction)
-  {
-  case FWD:
-    pwm_start(PWM_FWD, PWM_FREQ, pwm_duty_cycle, TimerCompareFormat_t::RESOLUTION_16B_COMPARE_FORMAT);
-    break;
-
-  case REV:
-    pwm_start(PWM_REV, PWM_FREQ, pwm_duty_cycle, TimerCompareFormat_t::RESOLUTION_16B_COMPARE_FORMAT);
-    break;
-  }
-}
+/**
+ * @brief Initialize LED pin as digital write.
+ * @param none
+ * @retval none
+ */
 
 void setup()
 {
+  //initialize LED digital pin as an output
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(PA7, INPUT); //analog input
+  pinMode(PWM_PIN, OUTPUT);
+  pinMode(PULSE_SONAR, OUTPUT);
+  pinMode(SONAR_SNS, INPUT);
   pinMode(PWM_FWD, OUTPUT);
-  pinMode(PWM_REV, OUTPUT);
-  pinMode(DUTY_CYCLE_ADJ, INPUT);
-  pinMode(DIRECTION_CTRL, INPUT);
+  pinMode(PWM_BACK, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("Serial comms up");
 
-  /*
-  //Setup LED Heartbeat hardware timer
-  heartbeat_timer.setOverflow(1, HERTZ_FORMAT); //set frequency of timer
-  heartbeat_timer.attachInterrupt(Heartbeat);
-  heartbeat_timer.resume();
-  */
+  //Serial.println("Serial comms up");
 
-  HB_TIM->setOverflow(5, HERTZ_FORMAT);
-  HB_TIM->attachInterrupt(Heartbeat);
-  HB_TIM->resume();
+  //digitalWrite(PWM_PIN, HIGH);
+  //digitalWrite(PULSE_SONAR, HIGH);
 
-  M1_PWM_TIM->setOverflow(10, HERTZ_FORMAT);
-  M1_PWM_TIM->attachInterrupt(testMotor);
-  M1_PWM_TIM->resume();
-
-  /*
-  //Setup MOTOR1 hardware timer
-  motor1_pwm_timer.setOverflow(25, HERTZ_FORMAT); //how often motor can sample changes
-  motor1_pwm_timer.attachInterrupt(UpdateMotor1State);
-  motor1_pwm_timer.resume();
-  */
 }
 
+/**
+ * @brief Turn LED on for 1 sec and off for 1 sec.
+ * @param none
+ * @retval none
+ */
 void loop()
-{
+{ 
+
+  /*16 bit format for duty cycle, 2^16 = 65535*/
+  //pwm_start(PWM_FWD, PWM_FREQ, 65535, TimerCompareFormat_t::RESOLUTION_16B_COMPARE_FORMAT);
+  /*
+  int duty_cycle_percent = 75;
+  float duty_cycle_fn = 65535 * (duty_cycle_percent / 100); //full scale
+  pwm_start(PWM_BACK, PWM_FREQ, 65535/2, TimerCompareFormat_t::RESOLUTION_16B_COMPARE_FORMAT);
+  */
+  pwm_start(PWM_FWD, PWM_FREQ, 65535/2, TimerCompareFormat_t::RESOLUTION_16B_COMPARE_FORMAT);
+  
+
+  uint32_t delay_ms = 1000;
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(delay_ms);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(delay_ms);
+
+
+  
+  //Reflectance (Tape) Analog Read
+  
+  /*
+  float reflectance_sns = analogRead(TAPE_SNS);
+  Serial.println(reflectance_sns);
+  */
+
+
+  //Pulse Infrared Beacon
+  //Infrared Beacon Analog Read
+
+  //float ir_sns = analogRead(IR_SNS);
+  //Serial.println(ir_sns);
+
+  //Pulse and Read Sonar Sensor
+  
+  //digitalWrite(PULSE_SONAR, LOW); //turn fet off, pull trig to 5V
+  //delayMicroseconds(10);
+  //digitalWrite(PULSE_SONAR, HIGH); //turn fet on, ground trig (end of pulse)
+  //delay(100);
+
 }
+
+
+
+
---
+++ test_code/main.cpp
@@ -0,0 +1,92 @@
+#include "Arduino.h"
+
+// Set LED_BUILTIN if it is not defined by Arduino framework
+#define LED_BUILTIN PC13
+#define PWM_FWD PA_3
+#define PWM_REV PA_7
+#define DUTY_CYCLE_ADJ PB1
+#define DIRECTION_CTRL PB10
+
+#define FULL_SCALE_ADC 1024
+#define FULL_SCALE_PWM 65535
+#define PWM_FREQ 100
+
+#define FWD 1
+#define REV 0
+
+HardwareTimer *HB_TIM = new HardwareTimer(TIM1);
+HardwareTimer *M1_PWM_TIM = new HardwareTimer(TIM2);
+volatile bool led_state = false;
+
+void Heartbeat()
+{
+  led_state = !led_state;
+  digitalWrite(LED_BUILTIN, led_state ? HIGH : LOW);
+}
+
+void testMotor()
+{
+  // placeholder fn
+}
+
+void UpdateMotor1State()
+{
+
+  // call this function every x times a second - how many?
+
+  int direction = digitalRead(DIRECTION_CTRL);
+
+  /* uncomment once you have a pot hooked up to this adc channel
+  float duty_cycle_percent = analogRead(DUTY_CYCLE_ADJ) / FULL_SCALE_ADC; //number between 0 - 1023
+  float pwm_duty_cycle = duty_cycle_percent * FULL_SCALE_PWM;
+  */
+
+  // comment this out when you've hooked up adc channel pot duty cycle control
+  float pwm_duty_cycle = FULL_SCALE_PWM * 0.5;
+
+  switch (direction)
+  {
+  case FWD:
+    pwm_start(PWM_FWD, PWM_FREQ, pwm_duty_cycle, TimerCompareFormat_t::RESOLUTION_16B_COMPARE_FORMAT);
+    break;
+
+  case REV:
+    pwm_start(PWM_REV, PWM_FREQ, pwm_duty_cycle, TimerCompareFormat_t::RESOLUTION_16B_COMPARE_FORMAT);
+    break;
+  }
+}
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(PWM_FWD, OUTPUT);
+  pinMode(PWM_REV, OUTPUT);
+  pinMode(DUTY_CYCLE_ADJ, INPUT);
+  pinMode(DIRECTION_CTRL, INPUT);
+
+  /*
+  //Setup LED Heartbeat hardware timer
+  heartbeat_timer.setOverflow(1, HERTZ_FORMAT); //set frequency of timer
+  heartbeat_timer.attachInterrupt(Heartbeat);
+  heartbeat_timer.resume();
+  */
+
+  HB_TIM->setOverflow(5, HERTZ_FORMAT);
+  HB_TIM->attachInterrupt(Heartbeat);
+  HB_TIM->resume();
+
+  M1_PWM_TIM->setOverflow(10, HERTZ_FORMAT);
+  M1_PWM_TIM->attachInterrupt(testMotor);
+  M1_PWM_TIM->resume();
+
+  /*
+  //Setup MOTOR1 hardware timer
+  motor1_pwm_timer.setOverflow(25, HERTZ_FORMAT); //how often motor can sample changes
+  motor1_pwm_timer.attachInterrupt(UpdateMotor1State);
+  motor1_pwm_timer.resume();
+  */
+}
+
+void loop()
+{
+}
