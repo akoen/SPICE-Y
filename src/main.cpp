09c6275b7db583819cc493ab92e81ace49436e92
akoen <arw.koen@gmail.com>
2022-07-06 16:04:50 -0700

    Goertzel algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include "Arduino.h"
 #include "HardwareTimer.h"
 
+#include <cmath> 
+
 #include "config.h"
 
 /* Private define ------------------------------------------------------------*/
-#define IR_SENS_NUM_READINGS 256
+#define IR_SENS_NUM_READINGS 2048
 
 /* Private variables ---------------------------------------------------------*/
 /* ADC handler declaration */
@@ -121,15 +123,18 @@ extern "C" void HAL_ADC_MspInit(ADC_HandleTypeDef *hadc)
 
   /*##-2- Configure peripheral GPIO ##########################################*/
   /* Configure GPIO pin of the selected ADC channel */
-  GPIO_InitStruct.Pin = ADC_CHANNEL_0;
-  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
-  GPIO_InitStruct.Pull = GPIO_PIN_0;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  // TODO: Remove HAL code if works
+  pinMode(PA0, INPUT_ANALOG);
+  // GPIO_InitStruct.Pin = ADC_CHANNEL_0;
+  // GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+  // GPIO_InitStruct.Pull = GPIO_PIN_0;
+  // HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  GPIO_InitStruct.Pin = ADC_CHANNEL_1;
-  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
-  GPIO_InitStruct.Pull = GPIO_PIN_1;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  // GPIO_InitStruct.Pin = ADC_CHANNEL_1;
+  // GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+  // GPIO_InitStruct.Pull = GPIO_PIN_1;
+  // HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  pinMode(PA1, INPUT_ANALOG);
 
   /*##-3- Configure the DMA ##################################################*/
   /* Configure DMA parameters */
@@ -259,14 +264,48 @@ extern "C" void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *)
 {
   HAL_ADC_Stop_DMA(&AdcHandle);
   DMA1DataAvailable = true;
+}
 
+template <class T> void deinterleave(T source[], T *dest[], uint16_t sourceLength, uint16_t numDest) {
+  for(int i = 0; i < sourceLength/numDest; i ++) {
+      for(int j = 0; j < numDest; j++) {
+        dest[j][i] = source[i*numDest+j];
+      }
+  }
+}
+
+float goertzel_mag(int numSamples, int targetFreq, int sampleFreq, uint16_t data[])
+{
+    int k = (int) (0.5 + numSamples * ((float) targetFreq / sampleFreq));
+    float omega = (2.0 * PI * k) / numSamples;
+    float sine = sin(omega);
+    float cosine = cos(omega);
+    float q0=0;
+    float q1=0;
+    float q2=0;
+
+    // First stage
+    for(int i=0; i<numSamples; i++)
+    {
+        q0 = 2.0 * cosine * q1 - q2 + data[i];
+        q2 = q1;
+        q1 = q0;
+    }
+
+    float scalingFactor = numSamples / 2.0;
+
+    // Second stage
+    float real = (q1 - q2 * cosine) / scalingFactor;
+    float imag = (q2 * sine) / scalingFactor;
+
+    float magnitude = sqrtf(real*real + imag*imag);
+    return magnitude;
 }
 
 // the setup routine runs once when you press reset:
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(PA4, INPUT_ANALOG);
   pinMode(PB0, OUTPUT);
 
   Serial.begin(115200, SERIAL_8N1);
@@ -275,29 +314,39 @@ void setup()
   timerHeartbeat.attachInterrupt(timerHeartbeatInterrupt);
   timerHeartbeat.resume();
 
-
   /* Configure the ADC peripheral */
   ADC_Config();
 
   /* Run the ADC calibration */
-  if (HAL_ADCEx_Calibration_Start(&AdcHandle) != HAL_OK)
-  {
-    /* Calibration Error */
-    Error_Handler();
-  }
+  HAL_ADCEx_Calibration_Start(&AdcHandle);
+
   /* Start ADC conversion on regular group with transfer by DMA */
   HAL_ADC_Start_DMA(&AdcHandle, (uint32_t *)DMA1Data, IR_SENS_NUM_READINGS);
   // HAL_TIM_Base_Start_IT(timerSensorReadFast.getHandle());
 }
 
-// the loop routine runs over and over again forever:
+float magAvg = 0;
+float alpha = 0.05;
 void loop()
 {
   if(DMA1DataAvailable) {
     digitalWrite(PB0, HIGH);
-    while (!Serial)
-      ;
-    Serial.write((uint8_t *) DMA1Data, sizeof(DMA1Data));
+
+    uint16_t pin1[IR_SENS_NUM_READINGS/2] = {0};
+    uint16_t pin2[IR_SENS_NUM_READINGS/2] = {0};
+
+    uint16_t *source[] = {pin1, pin2};
+
+    deinterleave<uint16_t>(DMA1Data, source, IR_SENS_NUM_READINGS, 2);
+
+    float mag = goertzel_mag(IR_SENS_NUM_READINGS/2, 10000, 13999999/(12.5+71.5), pin1);
+    magAvg = alpha * mag + (1-alpha) * magAvg;
+    while (!Serial);
+    // Serial.write((uint8_t *) pin1, sizeof(pin1));
+    Serial.println(magAvg);
+    // while (!Serial);
+    // Serial.write((uint8_t *) pin2, sizeof(pin2));
+    
     digitalWrite(PB0, LOW);
     HAL_ADC_Start_DMA(&AdcHandle, (uint32_t *)DMA1Data, IR_SENS_NUM_READINGS);
     DMA1DataAvailable = false;
