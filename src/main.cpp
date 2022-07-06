2e01815b5a8b7161bd163e08439e26b08df4ed00
akoen <arw.koen@gmail.com>
2022-07-05 17:34:20 -0700

    Working DMA single channel

--- src/main.cpp
+++ src/main.cpp
@@ -2,33 +2,21 @@
 #include "HardwareTimer.h"
 
 /* Private define ------------------------------------------------------------*/
-#define ADCCONVERTEDVALUES_BUFFER_SIZE ((uint32_t) 2)     /* Size of array containing ADC converted values */
-#define RANGE_12BITS                   ((uint32_t) 4095)    /* Max value with a full range of 12 bits */
+#define IR_SENS_NUM_READINGS 256
 
 /* Private variables ---------------------------------------------------------*/
 /* ADC handler declaration */
-ADC_HandleTypeDef    AdcHandle;
+ADC_HandleTypeDef AdcHandle;
 /* Variable containing ADC conversions results */
-__IO uint16_t   aADCxConvertedValues[ADCCONVERTEDVALUES_BUFFER_SIZE];
-/* Variable to report ADC analog watchdog status:   */
-/*   RESET <=> voltage into AWD window   */
-/*   SET   <=> voltage out of AWD window */
-uint8_t         ubAnalogWatchdogStatus = RESET;  /* Set into analog watchdog interrupt callback */
-
+uint16_t DMA1Data[IR_SENS_NUM_READINGS];
+bool DMA1DataAvailable = false;
 
 HardwareTimer timerHeartbeat(TIM1);
 bool ledOn = false;
-
-void timerHeartbeatInterrupt() {
+void timerHeartbeatInterrupt()
+{
   ledOn = !ledOn;
   digitalWrite(LED_BUILTIN, ledOn ? HIGH : LOW);
-}
-
-HardwareTimer timerSensorReadFast(TIM2);
-void timerSensorReadFastInterrupt() {
-  digitalWrite(PB0, HIGH);
-  Serial.println(aADCxConvertedValues[0]);
-  digitalWrite(PB0, LOW);
 }
 
 /**
@@ -63,9 +51,9 @@ extern "C" void DMA1_Channel1_IRQHandler(void)
 */
 extern "C" void HAL_ADC_MspInit(ADC_HandleTypeDef *hadc)
 {
-  GPIO_InitTypeDef          GPIO_InitStruct;
-  static DMA_HandleTypeDef  DmaHandle;
-  RCC_PeriphCLKInitTypeDef  PeriphClkInit;
+  GPIO_InitTypeDef GPIO_InitStruct;
+  static DMA_HandleTypeDef DmaHandle;
+  RCC_PeriphCLKInitTypeDef PeriphClkInit;
 
   /*##-1- Enable peripherals and GPIO Clocks #################################*/
   /* Enable clock of GPIO associated to the peripheral channels */
@@ -98,13 +86,13 @@ extern "C" void HAL_ADC_MspInit(ADC_HandleTypeDef *hadc)
   /* Configure DMA parameters */
   DmaHandle.Instance = DMA1_Channel1;
 
-  DmaHandle.Init.Direction           = DMA_PERIPH_TO_MEMORY;
-  DmaHandle.Init.PeriphInc           = DMA_PINC_DISABLE;
-  DmaHandle.Init.MemInc              = DMA_MINC_ENABLE;
-  DmaHandle.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;   /* Transfer from ADC by half-word to match with ADC configuration: ADC resolution 10 or 12 bits */
-  DmaHandle.Init.MemDataAlignment    = DMA_MDATAALIGN_HALFWORD;   /* Transfer to memory by half-word to match with buffer variable type: half-word */
-  DmaHandle.Init.Mode                = DMA_CIRCULAR;              /* DMA in circular mode to match with ADC configuration: DMA continuous requests */
-  DmaHandle.Init.Priority            = DMA_PRIORITY_HIGH;
+  DmaHandle.Init.Direction = DMA_PERIPH_TO_MEMORY;
+  DmaHandle.Init.PeriphInc = DMA_PINC_DISABLE;
+  DmaHandle.Init.MemInc = DMA_MINC_ENABLE;
+  DmaHandle.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD; /* Transfer from ADC by half-word to match with ADC configuration: ADC resolution 10 or 12 bits */
+  DmaHandle.Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;    /* Transfer to memory by half-word to match with buffer variable type: half-word */
+  DmaHandle.Init.Mode = DMA_CIRCULAR;                           /* DMA in circular mode to match with ADC configuration: DMA continuous requests */
+  DmaHandle.Init.Priority = DMA_PRIORITY_HIGH;
 
   /* Deinitialize  & Initialize the DMA for new transfer */
   HAL_DMA_DeInit(&DmaHandle);
@@ -119,7 +107,6 @@ extern "C" void HAL_ADC_MspInit(ADC_HandleTypeDef *hadc)
   /* Priority: high-priority */
   HAL_NVIC_SetPriority(DMA1_Channel1_IRQn, 1, 0);
   HAL_NVIC_EnableIRQ(DMA1_Channel1_IRQn);
-
 
   /* NVIC configuration for ADC interrupt */
   /* Priority: high-priority */
@@ -169,28 +156,18 @@ extern "C" void HAL_ADC_MspDeInit(ADC_HandleTypeDef *hadc)
 */
 static void ADC_Config(void)
 {
-  ADC_ChannelConfTypeDef   sConfig;
-  ADC_AnalogWDGConfTypeDef AnalogWDGConfig;
+  ADC_ChannelConfTypeDef sConfig;
 
   /* Configuration of ADCx init structure: ADC parameters and regular group */
   AdcHandle.Instance = ADC1;
-
-  AdcHandle.Init.DataAlign             = ADC_DATAALIGN_RIGHT;
-  AdcHandle.Init.ScanConvMode          = ADC_SCAN_DISABLE;              /* Sequencer disabled (ADC conversion on only 1 channel: channel set on rank 1) */
-#if defined ADC_TRIGGER_FROM_TIMER
-  AdcHandle.Init.ContinuousConvMode    = DISABLE;                       /* Continuous mode disabled to have only 1 conversion at each conversion trig */
-#else
-  AdcHandle.Init.ContinuousConvMode    = ENABLE;                        /* Continuous mode to have maximum conversion speed (no delay between conversions) */
-#endif
-  AdcHandle.Init.NbrOfConversion       = 1;                             /* Parameter discarded because sequencer is disabled */
-  AdcHandle.Init.DiscontinuousConvMode = DISABLE;                       /* Parameter discarded because sequencer is disabled */
-  AdcHandle.Init.NbrOfDiscConversion   = 1;                             /* Parameter discarded because sequencer is disabled */
-#if defined ADC_TRIGGER_FROM_TIMER
-  AdcHandle.Init.ExternalTrigConv      = ADC_EXTERNALTRIGCONV_Tx_TRGO;  /* Trig of conversion start done by external event */
-#else
+  AdcHandle.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  AdcHandle.Init.ScanConvMode = ADC_SCAN_DISABLE;                  /* Sequencer disabled (ADC conversion on only 1 channel: channel set on rank 1) */
+  AdcHandle.Init.ContinuousConvMode = ENABLE;                    /* Continuous mode to have maximum conversion speed (no delay between conversions) */
+  AdcHandle.Init.NbrOfConversion = 1;                             /* Parameter discarded because sequencer is disabled */
+  AdcHandle.Init.DiscontinuousConvMode = DISABLE;                 /* Parameter discarded because sequencer is disabled */
+  AdcHandle.Init.NbrOfDiscConversion = 1;                         /* Parameter discarded because sequencer is disabled */
+  // AdcHandle.Init.ExternalTrigConv = ADC_EXTERNALTRIGCONV_T3_TRGO; /* Trig of conversion start done by external event */
   AdcHandle.Init.ExternalTrigConv      = ADC_SOFTWARE_START;            /* Software start to trig the 1st conversion manually, without external event */
-#endif
-
 
   if (HAL_ADC_Init(&AdcHandle) != HAL_OK)
   {
@@ -203,44 +180,12 @@ static void ADC_Config(void)
   /*       conversion is out of the analog watchdog window selected (ADC IT   */
   /*       enabled), select sampling time and ADC clock with sufficient       */
   /*       duration to not create an overhead situation in IRQHandler.        */
-  sConfig.Channel      = ADC_CHANNEL_4;
-  sConfig.Rank         = ADC_REGULAR_RANK_1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_41CYCLES_5;
-  //sConfig.SamplingTime = ADC_SAMPLETIME_239CYCLES_5;
+  sConfig.Channel = ADC_CHANNEL_4;
+  sConfig.Rank = ADC_REGULAR_RANK_1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_71CYCLES_5;
+  // sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
 
-  if (HAL_ADC_ConfigChannel(&AdcHandle, &sConfig) != HAL_OK)
-  {
-    /* Channel Configuration Error */
-    Error_Handler();
-  }
-
-  /* Set analog watchdog thresholds in order to be between steps of DAC       */
-  /* voltage.                                                                 */
-  /*  - High threshold: between DAC steps 1/2 and 3/4 of full range:          */
-  /*                    5/8 of full range (4095 <=> Vdda=3.3V): 2559<=> 2.06V */
-  /*  - Low threshold:  between DAC steps 0 and 1/4 of full range:            */
-  /*                    1/8 of full range (4095 <=> Vdda=3.3V): 512 <=> 0.41V */
-
-  /* Analog watchdog 1 configuration */
-  AnalogWDGConfig.WatchdogMode = ADC_ANALOGWATCHDOG_ALL_REG;
-  AnalogWDGConfig.Channel = ADC_CHANNEL_4;
-  AnalogWDGConfig.ITMode = ENABLE;
-  AnalogWDGConfig.HighThreshold = (RANGE_12BITS * 5 / 8);
-  AnalogWDGConfig.LowThreshold = (RANGE_12BITS * 1 / 8);
-  if (HAL_ADC_AnalogWDGConfig(&AdcHandle, &AnalogWDGConfig) != HAL_OK)
-  {
-    /* Channel Configuration Error */
-    Error_Handler();
-  }
-}
-/**
-    @brief  Conversion complete callback in non blocking mode
-    @param  AdcHandle : AdcHandle handle
-    @note   This example shows a simple way to report end of conversion
-            and get convers100 Hzion result. You can add your own implementation.
-    @retval None
-*/
-extern "C" void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef * ) {
+  HAL_ADC_ConfigChannel(&AdcHandle, &sConfig);
 }
 
 /**
@@ -248,67 +193,62 @@ extern "C" void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef * ) {
     @param  hadc: ADC handle
     @retval None
 */
-extern "C" void HAL_ADC_ConvHalfCpltCallback(ADC_HandleTypeDef * ) {
-}
-
-/**
-    @brief  Analog watchdog callback in non blocking mode.
-    @param  hadc: ADC handle
-    @retval None
-*/
-extern "C" void HAL_ADC_LevelOutOfWindowCallback(ADC_HandleTypeDef * ) {
-  /* Set variable to report analog watchdog out of window status to main      */
-  /* program.                                                                 */
-  ubAnalogWatchdogStatus = SET;
-}
-
-/**
-    @brief  ADC error callback in non blocking mode
-           (ADC conversion with interruption or transfer by DMA)
-    @param  hadc: ADC handle
-    @retval None
-*/
-extern "C" void HAL_ADC_ErrorCallback(ADC_HandleTypeDef * )
+extern "C" void HAL_ADC_ConvHalfCpltCallback(ADC_HandleTypeDef *)
 {
-  /* In case of ADC error, call main error handler */
-  Error_Handler();
 }
+
+/**
+    @brief  Conversion complete callback in non blocking mode
+    @param  AdcHandle : AdcHandle handle
+    @note   This example shows a simple way to report end of conversion
+            and get convers100 Hzion result. You can add your own implementation.
+    @retval None
+*/
+extern "C" void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *)
+{
+  HAL_ADC_Stop_DMA(&AdcHandle);
+  DMA1DataAvailable = true;
+
+}
+
 // the setup routine runs once when you press reset:
-void setup() {
+void setup()
+{
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(PA4, INPUT_ANALOG);
   pinMode(PB0, OUTPUT);
 
-  Serial.begin(115200);
-
+  Serial.begin(115200, SERIAL_8N1);
 
   timerHeartbeat.setOverflow(2, HERTZ_FORMAT);
   timerHeartbeat.attachInterrupt(timerHeartbeatInterrupt);
   timerHeartbeat.resume();
 
-  timerSensorReadFast.setOverflow(40000, HERTZ_FORMAT);
-  timerSensorReadFast.attachInterrupt(timerSensorReadFastInterrupt);
-  timerSensorReadFast.resume();
 
   /* Configure the ADC peripheral */
   ADC_Config();
 
   /* Run the ADC calibration */
-  if (HAL_ADCEx_Calibration_Start(&AdcHandle) != HAL_OK) {
+  if (HAL_ADCEx_Calibration_Start(&AdcHandle) != HAL_OK)
+  {
     /* Calibration Error */
     Error_Handler();
   }
   /* Start ADC conversion on regular group with transfer by DMA */
-  if (HAL_ADC_Start_DMA(&AdcHandle,
-                        (uint32_t *)aADCxConvertedValues,
-                        ADCCONVERTEDVALUES_BUFFER_SIZE
-                       ) != HAL_OK) {
-    /* Start Error */
-    Error_Handler();
-  }
+  HAL_ADC_Start_DMA(&AdcHandle, (uint32_t *)DMA1Data, IR_SENS_NUM_READINGS);
+  // HAL_TIM_Base_Start_IT(timerSensorReadFast.getHandle());
 }
 
 // the loop routine runs over and over again forever:
-void loop() {
-
+void loop()
+{
+  if(DMA1DataAvailable) {
+    digitalWrite(PB0, HIGH);
+    while (!Serial)
+      ;
+    Serial.write((uint8_t *) DMA1Data, sizeof(DMA1Data));
+    digitalWrite(PB0, LOW);
+    HAL_ADC_Start_DMA(&AdcHandle, (uint32_t *)DMA1Data, IR_SENS_NUM_READINGS);
+    DMA1DataAvailable = false;
+  }
 }
