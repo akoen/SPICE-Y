1232efa0da485d10502a7328a2f94c0bf6e4e954
chrisyoon6 <chris7yoon@gmail.com>
2022-07-30 15:23:33 -0700

    added test files

---
+++ include/tests.h
@@ -0,0 +1,9 @@
+#include "config.h"
+/*
+ * This file is meant to implement a test framework for the robot, containing tests for both
+ * existing and new implementation.
+ * 
+ * Regression testing should be done after a new feature has been implmenented, to ensure that the feature is
+ * compatible with the robot's existing functionality.
+ */
+
