488dc3c6a4113f10b6273141f9647b53a6ebc53f
mjohal67 <majohal67@gmail.com>
2022-08-03 10:49:13 -0700

    schematic changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,40 @@
 #include <Arduino.h>
-#include <Servo.h>
 
-int clawServoPin = PB1;
-int armServoPin = PB0;
-Servo clawServo;
-Servo armServo;
+#define LED_BUILTIN PC13
+#define REFLECTANCE_PIN PA0
 
-void setup() {
-  clawServo.attach(clawServoPin);
-  armServo.attach(armServoPin);
+
+#define SERIAL_BAUD 9600
+#define TIMER1_OVERFLOW 200000 // microseconds
+#define PWM_HZ 10
+
+HardwareTimer *Timer1 = new HardwareTimer(TIM1);
+volatile bool led_state = false;
+
+void ISR_Heartbeat();
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(REFLECTANCE_PIN, INPUT);
+
+  Timer1->setOverflow(TIMER1_OVERFLOW, MICROSEC_FORMAT);
+  Timer1->attachInterrupt(ISR_Heartbeat);
+  Timer1->resume();
+
+  Serial.begin(SERIAL_BAUD);
 }
 
-void loop() {
-  Serial.println(2);
-  // //close claw
-  // clawServo.write(50);
-  // delay(1000);
+void loop()
+{
+  int laser_reflectance_analog = analogRead(REFLECTANCE_PIN); //higher analog value = less reflectance = larger "distance" from laser to object
+  Serial.print("ADC Reading: "); 
+  Serial.println(laser_reflectance_analog);
+  delay(500);
+}
 
-  // //lift arm
-  // armServo.write(115);
-  // delay(1000);
-
-  // //open Claw
-  // clawServo.write(100);
-  // delay(1000);
-  // clawServo.write(180);
-
-  // //lower Arm
-  // armServo.write(0);
-  // delay(1000);
+void ISR_Heartbeat()
+{
+  led_state = !led_state;
+  digitalWrite(LED_BUILTIN, led_state ? HIGH : LOW); // if led_state == true, go high, else, go low
 }
